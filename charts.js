f28d726cd61f61157ec31e5a93179e5f41a3db13
Bria <120140614+briagrhm@users.noreply.github.com>
2023-03-22 08:47:52 -0400

    Update charts.js

--- charts.js
+++ charts.js
@@ -153,11 +153,11 @@ function buildCharts(sample) {
           tickcolor: "black"},
         bar: {color: "black"},
         steps: [
-          {range: [0, 2], color: "red"},
-          {range: [2, 4], color: "orange"},
+          {range: [0, 2], color: "lightgreen"},
+          {range: [2, 4], color: "red"},
           {range: [4, 6], color: "yellow"},
-          {range: [6, 8], color: "lightgreen"},
-          {range: [8, 10], color: "green"},
+          {range: [6, 8], color: "orange"},
+          {range: [8, 10], color: "purple"},
         ]}
     }];
     
